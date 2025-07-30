79221003ec79856ffe32e80d16b1d0da47415013
Sav <maidenfan78@gmail.com>
2025-07-31 07:48:29 +1000

    bug fixes

--- babel.config.js
+++ babel.config.js
@@ -1,21 +1,6 @@
-// babel.config.js
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
-    plugins: [
-      // If you’re using Reanimated:
-      "react-native-reanimated/plugin",
-
-      // Dotenv support for @env
-      [
-        "module:react-native-dotenv",
-        {
-          moduleName: "@env",
-          path: ".env",
-          allowUndefined: true
-        }
-      ]
-    ]
+    presets: ['babel-preset-expo'],
   };
 };
